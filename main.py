23ae98e5a0e8254da31aab7183677b50b87b082c
kimball chey <kimball.chey@icloud.com>
2019-04-09 21:27:50 -0500

    First save

---
+++ main.py
@@ -0,0 +1,43 @@
+from caesar import rotate_string
+from flask import Flask, request
+
+app = Flask (__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    return """<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="post">
+            <div>
+                <label for="rot">Rotate by:</label>
+                <input type="text" name="rot" value="0">
+                <p class="error"></p>
+            </div>
+            <textarea type="text" name="text"></textarea>
+            <br>
+            <input type="submit" value="Submit Query">
+            </form>
+    </body>
+</html> """
+
+app.run()
